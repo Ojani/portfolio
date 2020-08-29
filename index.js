f570d9d7f92e6889dbbaccc2bebb18b9e642f80c
Ojani Figueroa <ojanifigueroa@Ojanis-MBP.lan>
2020-08-28 23:11:34 -0400

    Made the skills section and added hoverable skill cards.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ window.onscroll = () => {
 
     if(scrollY > 20) {
         document.querySelector(".nav-bar").classList.add("scrolled");
-    } else {
-        document.querySelector(".nav-bar").classList.remove("scrolled");
+        window.onscroll = undefined;
     }
 }
